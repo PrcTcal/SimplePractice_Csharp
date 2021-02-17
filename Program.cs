177080b6c90bd7cc93db6ed10df795fc3efca385
PrcTcal <hojjang0826@gmail.com>
2021-02-17 19:12:39 +0900

    convert function implementing(add finished, edit & delete left)

--- Program.cs
+++ Program.cs
@@ -44,47 +44,6 @@ namespace DynamoDB_intro
     }
     public partial class DdbIntro
     {
-        /*
-        // Document를 이용하여 scan하는 방식을 시도해보았으나 메모리 누수가 심하고 ProjectionExpression을 걸 수 있는 방법을 찾지 못하여 폐기함
-        public void test(){
-            Stopwatch sw = new Stopwatch();
-            sw.Start();
-            AppSettingsReader ar = new AppSettingsReader();
-            var credentials = new BasicAWSCredentials((string)ar.GetValue("accessKeyId", typeof(string)), (string)ar.GetValue("secretAccessKey", typeof(string)));
-            var client = new AmazonDynamoDBClient(credentials, RegionEndpoint.APNortheast2);
-            Table table = Table.LoadTable(client, "test01-music");
-
-            int totalSegments = 10;
-            for(int k = 0 ; k < 1 ; k++){
-                Parallel.For(k * totalSegments, (k + 1) * totalSegments, segment => {
-                    Console.WriteLine(segment);
-                    ScanFilter filter = new ScanFilter();
-                    List<AttributeValue> ft = new List<AttributeValue>();
-                    ft.Add(new AttributeValue{N = Convert.ToString(segment * 500)});
-                    ft.Add(new AttributeValue{N = Convert.ToString(((segment + 1) * 500) - 1)});
-                    
-                    filter.AddCondition("dummy", ScanOperator.Between, ft);
-                    
-                    Search search = table.Scan(filter);
-                    JArray ja = new JArray();
-                    do{
-                        var docList = search.GetNextSetAsync();
-                        docList.Result.ForEach(doc => {
-                            ja.Add(JsonConvert.DeserializeObject<JObject>(doc.ToJson()));
-                        });
-                        Console.WriteLine(segment + " - size : " + ja.Count);
-                    } while(!search.IsDone);
-                    Console.WriteLine(segment + " Done! : " + ja.Count);
-                    StreamWriter fs = new StreamWriter(new FileStream(String.Format("./exportData/export{0}.json", segment), FileMode.Create));
-                    fs.WriteLine(ja.ToString());
-                    fs.Close();
-                    Console.WriteLine("saved export{0}.json successfully!", segment);
-                });
-            }
-            sw.Stop();
-            Console.WriteLine(sw.ElapsedMilliseconds.ToString() + "ms");
-        }
-        */
 
         public void export(string tableName){
             Stopwatch sw = new Stopwatch();
@@ -95,6 +54,7 @@ namespace DynamoDB_intro
 
             Console.WriteLine("export process executed => ");
 
+            
             // 병렬 scan
             int totalSegments = 5;
             for(int k = 0 ; k < 2 ; k++){
@@ -124,7 +84,6 @@ namespace DynamoDB_intro
                         ScanResult result = client.Scan(request);
                         List<Dictionary<string, AttributeValue>> items = result.Items;
                         
-                        
                         foreach(Dictionary<string, AttributeValue> item in items){
                             try{
                                 music.id = item["id"].S;
@@ -141,7 +100,6 @@ namespace DynamoDB_intro
                                 jsonArray.Add(json);
                                 if(jsonArray.Count == 100000){
                                     Console.WriteLine("size : " + jsonArray.Count);
-                                    //File.WriteAllText(String.Format("./exportData/export{0}.json", segment), jsonArray.ToString());
                                     StreamWriter fs = new StreamWriter(new FileStream(String.Format("./exportData/export{0}.json", segment), FileMode.Create));
                                     fs.WriteLine(jsonArray.ToString());
                                     fs.Close();
@@ -152,11 +110,61 @@ namespace DynamoDB_intro
                             }
                         }
                         
-                        //Console.WriteLine(segment + " - size : " + jsonArray.Count);
+                        Console.WriteLine(segment + " - size : " + jsonArray.Count);
                         startKey = result.LastEvaluatedKey;
                     } while(startKey != null && startKey.Count > 0);
                 });
             }
+            
+
+            /*
+            // 단일 scan
+            Table table = Table.LoadTable(client, (string)ar.GetValue("tableName", typeof(string)));
+            Dictionary<string, AttributeValue> startKey = null;
+            MusicModel music = new MusicModel();
+            JArray jsonArray = new JArray();
+            int fileNum = 0;
+            
+            do{
+                ScanRequest request = new ScanRequest{
+                    TableName = tableName,
+                    ExclusiveStartKey = startKey,
+                    ProjectionExpression = "id, Artist, songTitle, info, actv, idx"
+                };
+                ScanResult result = client.Scan(request);
+                List<Dictionary<string, AttributeValue>> items = result.Items;
+                
+                foreach(Dictionary<string, AttributeValue> item in items){
+                    try{
+                        music.id = item["id"].S;
+                        music.Artist = item["Artist"].S;
+                        music.songTitle = item["songTitle"].S;
+                        Dictionary<string, string> dict = new Dictionary<string, string>();
+                        dict.Add("album", item["info"].M["album"].S);
+                        dict.Add("release", item["info"].M["release"].S);
+                        music.info = dict;
+                        music.idx = int.Parse(item["idx"].N);
+                        music.actv = item["actv"].BOOL;
+                        string jsonText = JsonConvert.SerializeObject(music, Formatting.None);
+                        JObject json = JObject.Parse(jsonText);
+                        jsonArray.Add(json);
+                        if(jsonArray.Count == 1000000){
+                            Console.WriteLine("size : " + jsonArray.Count);
+                            StreamWriter fs = new StreamWriter(new FileStream("./exportData/export.json", FileMode.Create));
+                            fs.WriteLine(jsonArray.ToString());
+                            fs.Close();
+                            Console.WriteLine("saved export.json successfully!");
+                            jsonArray = new JArray();
+                        }
+                    } catch(Exception e){
+                        Console.WriteLine(e.StackTrace);
+                    }
+                }
+                
+                Console.WriteLine("size : " + jsonArray.Count);
+                startKey = result.LastEvaluatedKey;
+            } while(startKey != null && startKey.Count > 0);
+            */
 
             sw.Stop();
             Console.WriteLine(sw.ElapsedMilliseconds.ToString() + "ms");
@@ -231,6 +239,84 @@ namespace DynamoDB_intro
             Console.WriteLine("import process terminated");
         }
 
+        public void convert(string convertOption){
+            Console.WriteLine("reading export.json...");
+            StreamReader sr = new StreamReader(new FileStream("./exportData/export7.json", FileMode.Open, FileAccess.Read));
+            JArray jsonArray = JsonConvert.DeserializeObject<JArray>(sr.ReadToEnd());
+            Console.WriteLine("read finished - item count : " + jsonArray.Count);
+            StreamWriter sw = new StreamWriter(new FileStream("./exportData/export7.json", FileMode.OpenOrCreate, FileAccess.Write));
+            switch(convertOption){
+
+                // 데이터 추가
+                case "add":
+                    string command;
+                    JObject inputData = new JObject();
+                    Console.WriteLine("[System] 추가하고자 하는 항목의 데이터를 입력해주세요");
+                    do {
+                        Console.WriteLine("[System] 입력 예시 : [type] [Field Name] [Field Value]. 입력을 중단하려면 done을 입력하세요.");
+                        Console.WriteLine(">> ");
+                        command = Console.ReadLine();
+                        string[] cmd = command.Split(" ");
+                        if(cmd[0] == "s"){
+                            inputData.Add(cmd[1], cmd[2]);
+                        } else if(cmd[0] == "n"){
+                            inputData.Add(cmd[1], Convert.ToInt32(cmd[2]));
+                        } else if(cmd[0] == "b"){
+                            inputData.Add(cmd[1], Convert.ToBoolean(cmd[2]));
+                        } else if(cmd[0] == "m"){
+                            string tempCommand;
+                            JObject info = new JObject();
+                            do{
+                                Console.WriteLine("[System] {0}의 항목을 입력하세요 : [type] [Field Name] [Field Value]. 입력을 중단하려면 done을 입력하세요.", cmd[0]);
+                                Console.WriteLine(">> ");
+                                tempCommand = Console.ReadLine();
+                                String[] tempCmd = tempCommand.Split(" ");
+                                if(tempCmd[0] == "s"){
+                                    info.Add(tempCmd[1], tempCmd[2]);
+                                } else if(tempCmd[0] == "n"){
+                                    info.Add(tempCmd[1], Convert.ToInt32(tempCmd[2]));
+                                } else if(tempCmd[0] == "b"){
+                                    info.Add(tempCmd[1], Convert.ToBoolean(tempCmd[2]));
+                                }
+                            } while(tempCommand != "done");
+                            inputData.Add(cmd[1], info);
+                        }
+                    } while(command != "done");
+                    Console.WriteLine(inputData);
+                    jsonArray.Add(inputData);
+                    sw.WriteLine(jsonArray.ToString());
+                    sw.Close();
+                    Console.WriteLine("added to export.json successfully!");
+                    break;
+
+                // 데이터 수정
+                case "edit":
+                    Console.WriteLine("[system] 수정하고자 하는 데이터의 id를 입력해주세요");
+                    Console.Write(">> ");
+                    command = Console.ReadLine();
+                    JObject target = new JObject();
+                    foreach(JObject json in jsonArray){
+                        if(json.GetValue("id").ToString() == command){
+                            target = json;
+                        }
+                    }
+                    Console.WriteLine(target);
+
+                    // 수정할 field와 그 값 입력받아서 target의 값 수정
+                    break;
+
+
+
+
+
+                // 데이터 삭제
+                case "delete":
+                    // edit과 마찬가지로 id로 찾고 해당 데이터 제외한 나머지 다시 저장
+                    break;
+            }
+            return;
+        }
+
         static void Main(string[] args)
         {
             DdbIntro db = new DdbIntro();
@@ -255,6 +341,10 @@ namespace DynamoDB_intro
                         } else {
                             Console.WriteLine("incorrect command. use [import] [TableName]");  
                         }    
+                        break;
+                    case "convert":
+                        db.convert(cmd[1]);
+                        GC.Collect();
                         break;
                 }
             }
