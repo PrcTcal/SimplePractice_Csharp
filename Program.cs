afc469ef7ebab26875f1cf0e3248318d08a1c899
PrcTcal <hojjang0826@gmail.com>
2021-02-17 14:44:49 +0900

    export test using DynamoDBv2.DocumentModel

--- Program.cs
+++ Program.cs
@@ -42,10 +42,51 @@ namespace DynamoDB_intro
         }
 
     }
-    public static partial class DdbIntro
+    public partial class DdbIntro
     {
+        /*
+        // Document를 이용하여 scan하는 방식을 시도해보았으나 메모리 누수가 심하고 ProjectionExpression을 걸 수 있는 방법을 찾지 못하여 폐기함
+        public void test(){
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            AppSettingsReader ar = new AppSettingsReader();
+            var credentials = new BasicAWSCredentials((string)ar.GetValue("accessKeyId", typeof(string)), (string)ar.GetValue("secretAccessKey", typeof(string)));
+            var client = new AmazonDynamoDBClient(credentials, RegionEndpoint.APNortheast2);
+            Table table = Table.LoadTable(client, "test01-music");
 
-        public static void export(string tableName){
+            int totalSegments = 10;
+            for(int k = 0 ; k < 1 ; k++){
+                Parallel.For(k * totalSegments, (k + 1) * totalSegments, segment => {
+                    Console.WriteLine(segment);
+                    ScanFilter filter = new ScanFilter();
+                    List<AttributeValue> ft = new List<AttributeValue>();
+                    ft.Add(new AttributeValue{N = Convert.ToString(segment * 500)});
+                    ft.Add(new AttributeValue{N = Convert.ToString(((segment + 1) * 500) - 1)});
+                    
+                    filter.AddCondition("dummy", ScanOperator.Between, ft);
+                    
+                    Search search = table.Scan(filter);
+                    JArray ja = new JArray();
+                    do{
+                        var docList = search.GetNextSetAsync();
+                        docList.Result.ForEach(doc => {
+                            ja.Add(JsonConvert.DeserializeObject<JObject>(doc.ToJson()));
+                        });
+                        Console.WriteLine(segment + " - size : " + ja.Count);
+                    } while(!search.IsDone);
+                    Console.WriteLine(segment + " Done! : " + ja.Count);
+                    StreamWriter fs = new StreamWriter(new FileStream(String.Format("./exportData/export{0}.json", segment), FileMode.Create));
+                    fs.WriteLine(ja.ToString());
+                    fs.Close();
+                    Console.WriteLine("saved export{0}.json successfully!", segment);
+                });
+            }
+            sw.Stop();
+            Console.WriteLine(sw.ElapsedMilliseconds.ToString() + "ms");
+        }
+        */
+
+        public void export(string tableName){
             Stopwatch sw = new Stopwatch();
             sw.Start();
             AppSettingsReader ar = new AppSettingsReader();
@@ -55,117 +96,75 @@ namespace DynamoDB_intro
             Console.WriteLine("export process executed => ");
 
             // 병렬 scan
-            int totalSegments = 10;
-            Parallel.For(0, totalSegments, segment => {
-                Console.WriteLine("segment : " + segment);
-                Table table = Table.LoadTable(client, (string)ar.GetValue("tableName", typeof(string)));
-                Dictionary<string, AttributeValue> startKey = null;
-                MusicModel music = new MusicModel();
-                JArray jsonArray = new JArray();
-                
-                do{
-                    ScanRequest request = new ScanRequest{
-                        TableName = tableName,
-                        ExclusiveStartKey = startKey,
-                        ProjectionExpression = "id, Artist, songTitle, info, actv, idx",
-                        FilterExpression = "dummy between :v_start and :v_end",
-                        ExpressionAttributeValues = new Dictionary<string, AttributeValue> {
-                            {":v_start", new AttributeValue {
-                                N = Convert.ToString(segment * 500)
-                            }},
-                            {":v_end", new AttributeValue {
-                                N = Convert.ToString(((segment + 1) * 500) - 1)
-                            }}
-                        }
-                    };
-
-                    ScanResult result = client.Scan(request);
-                    List<Dictionary<string, AttributeValue>> items = result.Items;
+            int totalSegments = 5;
+            for(int k = 0 ; k < 2 ; k++){
+                Parallel.For(k * totalSegments, (k + 1) * totalSegments, segment => {
+                    //Console.WriteLine("segment : " + segment);
+                    Table table = Table.LoadTable(client, tableName);
+                    Dictionary<string, AttributeValue> startKey = null;
+                    MusicModel music = new MusicModel();
+                    JArray jsonArray = new JArray();
                     
-                    foreach(Dictionary<string, AttributeValue> item in items){
-                        try{
-                            music.id = item["id"].S;
-                            music.Artist = item["Artist"].S;
-                            music.songTitle = item["songTitle"].S;
-                            Dictionary<string, string> dict = new Dictionary<string, string>();
-                            dict.Add("album", item["info"].M["album"].S);
-                            dict.Add("release", item["info"].M["release"].S);
-                            music.info = dict;
-                            music.idx = int.Parse(item["idx"].N);
-                            music.actv = item["actv"].BOOL;
-                            string jsonText = JsonConvert.SerializeObject(music, Formatting.None);
-                            JObject json = JObject.Parse(jsonText);
-                            jsonArray.Add(json);
-                            if(jsonArray.Count == 100000){
-                                Console.WriteLine("size : " + jsonArray.Count);
-                                File.WriteAllText(String.Format("./exportData/export{0}.json", segment), jsonArray.ToString());
-                                Console.WriteLine("saved export{0}.json successfully!", segment);
+                    do{
+                        ScanRequest request = new ScanRequest{
+                            TableName = tableName,
+                            ExclusiveStartKey = startKey,
+                            ProjectionExpression = "id, Artist, songTitle, info, actv, idx",
+                            FilterExpression = "dummy between :v_start and :v_end",
+                            ExpressionAttributeValues = new Dictionary<string, AttributeValue> {
+                                {":v_start", new AttributeValue {
+                                    N = Convert.ToString(segment * 500)
+                                }},
+                                {":v_end", new AttributeValue {
+                                    N = Convert.ToString(((segment + 1) * 500) - 1)
+                                }}
                             }
-                        } catch(Exception e){
-                            Console.WriteLine(e.StackTrace);
+                        };
+                        
+                        ScanResult result = client.Scan(request);
+                        List<Dictionary<string, AttributeValue>> items = result.Items;
+                        
+                        
+                        foreach(Dictionary<string, AttributeValue> item in items){
+                            try{
+                                music.id = item["id"].S;
+                                music.Artist = item["Artist"].S;
+                                music.songTitle = item["songTitle"].S;
+                                Dictionary<string, string> dict = new Dictionary<string, string>();
+                                dict.Add("album", item["info"].M["album"].S);
+                                dict.Add("release", item["info"].M["release"].S);
+                                music.info = dict;
+                                music.idx = int.Parse(item["idx"].N);
+                                music.actv = item["actv"].BOOL;
+                                string jsonText = JsonConvert.SerializeObject(music, Formatting.None);
+                                JObject json = JObject.Parse(jsonText);
+                                jsonArray.Add(json);
+                                if(jsonArray.Count == 100000){
+                                    Console.WriteLine("size : " + jsonArray.Count);
+                                    //File.WriteAllText(String.Format("./exportData/export{0}.json", segment), jsonArray.ToString());
+                                    StreamWriter fs = new StreamWriter(new FileStream(String.Format("./exportData/export{0}.json", segment), FileMode.Create));
+                                    fs.WriteLine(jsonArray.ToString());
+                                    fs.Close();
+                                    Console.WriteLine("saved export{0}.json successfully!", segment);
+                                }
+                            } catch(Exception e){
+                                Console.WriteLine(e.StackTrace);
+                            }
                         }
-                    }
-                    
-                    Console.WriteLine(segment + " - size : " + jsonArray.Count);
-                    startKey = result.LastEvaluatedKey;
-                } while(startKey != null && startKey.Count > 0);
-            });
+                        
+                        //Console.WriteLine(segment + " - size : " + jsonArray.Count);
+                        startKey = result.LastEvaluatedKey;
+                    } while(startKey != null && startKey.Count > 0);
+                });
+            }
 
-            /*
-            // 단일 scan
-            Table table = Table.LoadTable(client, (string)ar.GetValue("tableName", typeof(string)));
-            Dictionary<string, AttributeValue> startKey = null;
-            MusicModel music = new MusicModel();
-            JArray jsonArray = new JArray();
-            int fileNum = 0;
-            
-            do{
-                ScanRequest request = new ScanRequest{
-                    TableName = tableName,
-                    ExclusiveStartKey = startKey,
-                    ProjectionExpression = "id, Artist, songTitle, info, actv, idx"
-                };
-
-                ScanResult result = client.Scan(request);
-                List<Dictionary<string, AttributeValue>> items = result.Items;
-                
-                foreach(Dictionary<string, AttributeValue> item in items){
-                    try{
-                        music.id = item["id"].S;
-                        music.Artist = item["Artist"].S;
-                        music.songTitle = item["songTitle"].S;
-                        Dictionary<string, string> dict = new Dictionary<string, string>();
-                        dict.Add("album", item["info"].M["album"].S);
-                        dict.Add("release", item["info"].M["release"].S);
-                        music.info = dict;
-                        music.idx = int.Parse(item["idx"].N);
-                        music.actv = item["actv"].BOOL;
-                        string jsonText = JsonConvert.SerializeObject(music, Formatting.None);
-                        JObject json = JObject.Parse(jsonText);
-                        jsonArray.Add(json);
-                        if(jsonArray.Count == 100000){
-                            Console.WriteLine("size : " + jsonArray.Count);
-                            File.WriteAllText(String.Format("./exportData/export{0}.json", fileNum), jsonArray.ToString());
-                            Console.WriteLine("saved export{0}.json successfully!", fileNum++);
-                            jsonArray = new JArray();
-                        }
-                    } catch(Exception e){
-                        Console.WriteLine(e.StackTrace);
-                    }
-                }
-                
-                Console.WriteLine("size : " + jsonArray.Count);
-                
-                startKey = result.LastEvaluatedKey;
-            } while(startKey != null && startKey.Count > 0);
-            */
             sw.Stop();
             Console.WriteLine(sw.ElapsedMilliseconds.ToString() + "ms");
             Console.WriteLine("export process terminated");
-            
+            return;
         }
 
-        public static void import(string tableName){
+        public void import(string tableName){
             Stopwatch sw = new Stopwatch();
             sw.Start();
             AppSettingsReader ar = new AppSettingsReader();
@@ -234,6 +233,7 @@ namespace DynamoDB_intro
 
         static void Main(string[] args)
         {
+            DdbIntro db = new DdbIntro();
             string cmdLine = null;
             while(cmdLine != "exit"){
                 Console.Write(">> ");
@@ -242,14 +242,16 @@ namespace DynamoDB_intro
                 switch(cmd[0]){
                     case "export":
                         if(cmd.Length == 2){
-                            export(cmd[1]);
+                            Console.WriteLine("export");
+                            db.export(cmd[1]);
+                            GC.Collect();
                         } else {
                             Console.WriteLine("incorrect command. use [export] [TableName]");
                         }
                         break;
                     case "import":
                         if(cmd.Length == 2){
-                            import(cmd[1]);
+                            db.import(cmd[1]);
                         } else {
                             Console.WriteLine("incorrect command. use [import] [TableName]");  
                         }    
